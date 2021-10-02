2ca47afa116523fe1b03724e6f9e2c3ec9baddb6
heidi <heidi.poels@gmail.com>
2021-10-02 20:28:13 +0300

    initialize for search field.

--- search.js
+++ search.js
@@ -1,5 +1,14 @@
 let smileys;
 
+function initialize() {
+    (async () => {
+        const searchField = document.getElementById('search');
+        smileys = await getJSON(config.baseURL + 'emojis');
+        searchField.disabled = false;
+        searchField.value = '';
+    })();
+}
+
 function search() {
     const searchTerm = document.getElementById('search').value;
     const foundIt = smileys.hasOwnProperty(searchTerm);
